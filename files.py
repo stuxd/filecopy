00994e18c36e72ffa658668223b97983110300de
stuxd <stuartoryan@gmail.com>
2016-06-21 19:36:26 +1000

    file.py

--- files.py
+++ files.py
@@ -6,21 +6,21 @@ src = 'c:/python/from'
 dest = 'c:/python/to'
 invoicelist = 'c:/python/invoices.txt'
 
-def invoice_check(b):
+def invoice_check(inv):
 	with open(invoicelist,'r') as file:
 		for line in file:
-			if b in line:
+			if inv in line:
 				return True
 
-def name_check(name):
-	na = re.findall(r'\d{6}',name)
-	for b in na:
-		if invoice_check(b):
+def file_split(name):
+	split = re.findall(r'\d{6}',name)
+	for a in split:
+		if invoice_check(a):
 			return True
 
 for root, dirs, files in os.walk(src):
 	for names in files:
-		n = ''.join(root+"/"+names)
-		if name_check(names):
-			shutil.copy2(n,dest)
+		filepath = ''.join(root+"/"+names)
+		if file_split(names):
+			shutil.copy2(filepath,dest)
 			print 'Copying:',names
